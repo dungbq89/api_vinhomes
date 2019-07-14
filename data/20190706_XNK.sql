2a00982ce50e6932f1f3c43d5ca665ddeb12a9be
dungbq89 <dungbq89@gmail.com>
2019-07-14 11:04:48 +0700

    cấu hình data base

--- data/20190706_XNK.sql
+++ data/20190706_XNK.sql
@@ -1,6 +1,6 @@
 /*
 SQLyog Professional v12.09 (64 bit)
-MySQL - 5.1.73 : Database - dung_tiengtrunganz
+MySQL - 5.1.73 : Database - api_vinhomes
 *********************************************************************
 */
 
@@ -12,9 +12,9 @@ MySQL - 5.1.73 : Database - dung_tiengtrunganz
 /*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-CREATE DATABASE /*!32312 IF NOT EXISTS*/`dung_tiengtrunganz` /*!40100 DEFAULT CHARACTER SET utf8 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`api_vinhomes` /*!40100 DEFAULT CHARACTER SET utf8 */;
 
-USE `dung_tiengtrunganz`;
+USE `api_vinhomes`;
 
 /*Table structure for table `ad_activity` */
 
