e2e20236e193a44899abd5d8685e306132755257
dungbq89 <dungbq89@gmail.com>
2019-07-15 23:27:59 +0700

    page gioi thieu

---
+++ apps/frontend/modules/pageHtml/actions/actions.class.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * pageHome actions.
+ *
+ * @package    VTP2.0
+ * @subpackage pageHome
+ * @author     Your name here
+ * @version    SVN: $Id: actions.class.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class pageHtmlActions extends sfActions
+{
+    public function executeIndex(sfWebRequest $request)
+    {
+
+    }
+
+    public function executeIntroduction(sfWebRequest $request)
+    {
+
+    }
+}
